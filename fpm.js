cf9a752bbc361fbace6bc85744d385ef3444d50f
jake <jake@speedphp.com>
2017-02-20 17:32:51 +0800

    new 0.0.2

---
+++ fpm.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+var http=require('http');  
+var server=new http.Server(); 
+
+var d = function(){
+    this.response = []
+}
+
+d.prototype.write = function(str){
+    this.response.push(str)
+}
+
+server.on('request',function(req, res){  
+    res.writeHead(200,{'Content-Type':'text/html'});  
+    console.log(req.url);
+    
+    var uri = req.url == "/" ? "/index.jhtml" : req.url;
+    
+    fs.readFile('.'+uri, (err, data) => {
+        if (err){
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("404 error! File not found.");
+        }else{
+        
+            var da = new d
+            
+            var tpl = data.toString();
+            
+            var start_tag = new RegExp("<\\?js","igm");
+            var end_tag = new RegExp("\\?>","igm");
+            
+            var cookie = require("./lib/in.js");
+            
+            cookie.set("hello", "world");
+            
+            console.log(cookie.get("hello"))
+            
+            tpl = tpl.replace(start_tag, "`);");
+            tpl = tpl.replace(end_tag, "da.write(`");
+            
+            var runcode = "da.write(`" + tpl + "`);";
+            
+            console.log(runcode);
+            eval(runcode) 
+            res.end(da.response.join("\n"));  
+        }
+    });
+});  
+
+server.listen(3000);  
+console.log('HTTP SERVER is LISTENING AT PORT 3000.');  
+
---
+++ readme.md
@@ -0,0 +1,45 @@
+# A FastCGI Process Manager for NodeJS
+
+## TODO LIST
+
+- Exist file output?
+- include implementation
+- jhtml support optimization 
+- webapps config support
+- test readonly website buiding
+- cookie、session
+- header
+- sqlite
+- in.post，get，upload，query
+- out.write，end，redirect
+- util.html_escape，file_get，file_put
+- document
+- demo
+- publish
+
+## Architecture
+
+cookie
+
+session
+
+header
+
+sqlite
+
+in
+- post
+- get
+- upload
+- query
+	
+out
+- write
+- end
+- redirect
+
+util
+- html_escape
+- file_get
+- file_put
+	
