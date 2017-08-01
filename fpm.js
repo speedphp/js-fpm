5260d7624bf74a5e4bf5bde0f562d3ce5b51ab4b
jake <jake@speedphp.com>
2017-08-01 11:40:14 +0800

    output is finished, create a module

--- fpm.js
+++ fpm.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 var http=require('http');  
-var server=new http.Server(); 
-
+var server=new http.Server();
+var mime = require('mime-types')
+var path = require("path");
 var d = function(){
     this.response = []
 }
 
 d.prototype.write = function(str){
     this.response.push(str)
+}
+d.prototype.join = function (pattern) {
+    return this.response.join(pattern)
 }
 
 server.on('request',function(req, res){  
@@ -16,37 +20,31 @@ server.on('request',function(req, res){
     
     var uri = req.url == "/" ? "/index.jhtml" : req.url;
     
-    fs.readFile('.'+uri, (err, data) => {
+    fs.readFile('.'+uri, function(err, data) {
         if (err){
             res.writeHead(404, { "Content-Type": "text/plain" });
             res.end("404 error! File not found.");
         }else{
-        
-            var da = new d
-            
-            var tpl = data.toString();
-            
-            var start_tag = new RegExp("<\\?js","igm");
-            var end_tag = new RegExp("\\?>","igm");
-            
-            //var cookie = require("./lib/in.js");
-            
-            //cookie.set("hello", "world");
-            
-            //console.log(cookie.get("hello"))
-            
-            tpl = tpl.replace(start_tag, "`);");
-            tpl = tpl.replace(end_tag, "da.write(`");
-            
-            var runcode = "da.write(`" + tpl + "`);";
-            
-            console.log(runcode);
-            eval(runcode) 
-            res.end(da.response.join("\n"));  
+            var file = fs.realpathSync('.'+uri);
+            if(".jhtml" != path.extname(file)){
+                var type = mime.lookup(file) || 'application/octet-stream';
+                res.writeHead(200, { "Content-Type": type });
+                var readStream = fs.createReadStream(file);
+                readStream.pipe(res);
+            }else{
+                var da = new d
+                var tpl = data.toString();
+                var start_tag = new RegExp("<\\?js","igm");
+                var end_tag = new RegExp("\\?>","igm");
+                // var invalue = require("./lib/in.js");
+                // invalue.setValue("hello", "world");
+                // console.log(invalue.getValue("hello"))
+                tpl = tpl.replace(start_tag, "`);");
+                tpl = tpl.replace(end_tag, "da.write(`");
+                eval("da.write(`" + tpl + "`);")
+                res.end(da.join("\n"));
+            }
         }
     });
-});  
-
-server.listen(process.env.PORT);  
-console.log('HTTP SERVER is LISTENING AT PORT '+argv.port);  
+}).listen(3000);
 
